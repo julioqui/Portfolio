628a58d7935e3c88f36d2e50c372794b6b5eedad
Julio Quiezi <julioquiezi@gmail.com>
2022-10-20 22:08:23 +0900

    feat: add links to header

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "../styles/header.css";
+
 const Header = () => {
     return (
-        <>
-            
-        </>
-      );
+      <header>
+        <nav className="nav">
+            <Link to="/"><><button>About me</button></></Link>
+            <Link to="/resume"><button>Curriculum</button></Link>
+            <button>Contact</button>
+        </nav>
+      </header>
+    );
 }
-
 
 export default Header;
